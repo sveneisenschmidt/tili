c80894403fe8a373268190350292c8afbf58c024
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2024-09-26 18:54:29 +0200

    added based path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tili/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
